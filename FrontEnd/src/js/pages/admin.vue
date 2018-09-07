8bca2b5fc86757b73606d015d49ca3abb40b5c43
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2018-09-07 21:05:01 +0800

    Fixed

--- FrontEnd/src/js/pages/admin.vue
+++ FrontEnd/src/js/pages/admin.vue
@@ -1,6 +1,7 @@
 <template>
-<div>
-	<router-view></router-view>
-	<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.css.map">
-</div>
+<router-view></router-view>
 </template>
+<style>
+@import url(https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.css.map)
+</style>
+
