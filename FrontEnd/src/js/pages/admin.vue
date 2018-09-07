7fb0dc7c124ebacb35ee58108b42205b68b7dfc3
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2018-09-07 15:10:39 +0800

    Remove Front end

--- FrontEnd/src/js/pages/admin.vue
+++
@@ -1,6 +0,0 @@
-<template>
-<router-view></router-view>
-</template>
-<style>
-@import '~bulma/css/bulma.css';
-</style>
--- FrontEnd/src/js/plugins/l-notify/app.js
+++
@@ -1,9 +0,0 @@
-const lNotify = {
-	install: function (Vue, options) {
-		Vue.prototype.$lnotify = function(config) {
-			
-		}
-	}
-}
-
-export default lNotify;
